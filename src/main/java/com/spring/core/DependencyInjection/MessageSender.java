1cbb255874ecf55f9ef14bd5d3e6cae17e3d10e5
Shay-Bas <shaydabas@gmail.com>
2024-07-30 14:27:44 +0530

    Constructor based dependencyInjection

--- src/main/java/com/spring/core/DependencyInjection/MessageSender.java
+++ src/main/java/com/spring/core/DependencyInjection/MessageSender.java
@@ -7,14 +7,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class MessageSender {
     private MessageService messageService;
+    private MessageService smsService;
 
-    @Autowired
+    //@Autowired: not required after spring 4.3 onwards if only one constructor in a class
     public MessageSender(@Qualifier("emailService") MessageService messageService){
         this.messageService = messageService;
     }
 
+    @Autowired
+    public MessageSender(@Qualifier("emailService") MessageService messageService,@Qualifier("smsService") MessageService smsService){
+        this.messageService = messageService;
+        this.smsService = smsService;
+    }
+
     public void sendMessage(String message){
         this.messageService.sendMessage(message);
+        this.smsService.sendMessage(message);
     }
 
 }
